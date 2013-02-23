d85a65292409431127257cbdf749836669996a74
Thilo Brandt <mail@thilo-brandt.de>
2013-02-23 14:11:23 +0100

    [5.0.37] [tellows] added mass action for journal

--- modules/tellows/src/de/janrufmonitor/ui/jface/application/tellows/action/ScoreNumberTellows.java
+++ modules/tellows/src/de/janrufmonitor/ui/jface/application/tellows/action/ScoreNumberTellows.java
@@ -1,6 +1,7 @@
 package de.janrufmonitor.ui.jface.application.tellows.action;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.Iterator;
 import java.util.logging.Level;
 
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -69,66 +70,68 @@ public class ScoreNumberTellows extends AbstractAction {
 		if (v!=null) {
 			IStructuredSelection selection = (IStructuredSelection) v.getSelection();
 			if (!selection.isEmpty()) {
-				Object o1 = selection.getFirstElement();
-				Object o = null;
-				if (o1 instanceof ICall) {
-					o = ((ICall)o1).getCaller();
-				}
-				if (o1 instanceof ICaller) {
-					o = o1;
-				}
-				if (o!=null) { 
-					if (!((ICaller) o).getPhoneNumber().isClired() && 
-						!PhonenumberInfo.isInternalNumber(((ICaller) o).getPhoneNumber()) &&
-						(((ICaller) o).getPhoneNumber().getIntAreaCode().equalsIgnoreCase("49") ||
-								((ICaller) o).getPhoneNumber().getIntAreaCode().equalsIgnoreCase("41")||
-								((ICaller) o).getPhoneNumber().getIntAreaCode().equalsIgnoreCase("43"))
-					   ) {
-						
-						final Object o2 = o1;
-						final Object o3 = o;
-						
-						ProgressMonitorDialog pmd = new ProgressMonitorDialog(
-								DisplayManager.getDefaultDisplay().getActiveShell());
-						try {
-							IRunnableWithProgress r = new IRunnableWithProgress() {
-								public void run(IProgressMonitor progressMonitor) {
-									progressMonitor.beginTask(getI18nManager()
-											.getString(getNamespace(),
-													"scoring", "label",
-													getLanguage()),
-											IProgressMonitor.UNKNOWN);
-
-									progressMonitor.worked(1);
-
-									IAttributeMap m = TellowsProxy.getInstance().getTellowsData(((ICaller) o3).getPhoneNumber().getTelephoneNumber(), 
-											((ICaller) o3).getPhoneNumber().getIntAreaCode());
-									if (m.size()>0) {
-										((ICaller) o3).getAttributes().addAll(m);
-										if (m_app.getController() instanceof IExtendedApplicationController)
-											((IExtendedApplicationController)m_app.getController()).updateElement(o2, false);
-										else
-											m_app.getController().updateElement(o2);
-									}	
-
-									progressMonitor.done();
-								}
-							};
-							pmd.setBlockOnOpen(false);
-							pmd.run(true, false, r);
-
-							// ModalContext.run(r, true, pmd.getProgressMonitor(),
-							// DisplayManager.getDefaultDisplay());
-						} catch (InterruptedException e) {
-							m_logger.log(Level.SEVERE, e.getMessage(), e);
-						} catch (InvocationTargetException e) {
-							m_logger.log(Level.SEVERE, e.getMessage(), e);
-						}
-						this.m_app.updateViews(true);
-						
-						this.m_app.updateViews(true);
+				Iterator iter = selection.iterator();
+				while (iter.hasNext()) {
+					Object o1 = iter.next();
+					Object o = null;
+					if (o1 instanceof ICall) {
+						o = ((ICall)o1).getCaller();
 					}
-				}				
+					if (o1 instanceof ICaller) {
+						o = o1;
+					}
+					if (o!=null) { 
+						if (!((ICaller) o).getPhoneNumber().isClired() && 
+							!PhonenumberInfo.isInternalNumber(((ICaller) o).getPhoneNumber()) &&
+							(((ICaller) o).getPhoneNumber().getIntAreaCode().equalsIgnoreCase("49") ||
+									((ICaller) o).getPhoneNumber().getIntAreaCode().equalsIgnoreCase("41")||
+									((ICaller) o).getPhoneNumber().getIntAreaCode().equalsIgnoreCase("43"))
+						   ) {
+							
+							final Object o2 = o1;
+							final Object o3 = o;
+							
+							ProgressMonitorDialog pmd = new ProgressMonitorDialog(
+									DisplayManager.getDefaultDisplay().getActiveShell());
+							try {
+								IRunnableWithProgress r = new IRunnableWithProgress() {
+									public void run(IProgressMonitor progressMonitor) {
+										progressMonitor.beginTask(getI18nManager()
+												.getString(getNamespace(),
+														"scoring", "label",
+														getLanguage()),
+												IProgressMonitor.UNKNOWN);
+	
+										progressMonitor.worked(1);
+	
+										IAttributeMap m = TellowsProxy.getInstance().getTellowsData(((ICaller) o3).getPhoneNumber().getTelephoneNumber(), 
+												((ICaller) o3).getPhoneNumber().getIntAreaCode());
+										if (m.size()>0) {
+											((ICaller) o3).getAttributes().addAll(m);
+											if (m_app.getController() instanceof IExtendedApplicationController)
+												((IExtendedApplicationController)m_app.getController()).updateElement(o2, false);
+											else
+												m_app.getController().updateElement(o2);
+										}	
+	
+										progressMonitor.done();
+									}
+								};
+								pmd.setBlockOnOpen(false);
+								pmd.run(true, false, r);
+	
+								// ModalContext.run(r, true, pmd.getProgressMonitor(),
+								// DisplayManager.getDefaultDisplay());
+							} catch (InterruptedException e) {
+								m_logger.log(Level.SEVERE, e.getMessage(), e);
+							} catch (InvocationTargetException e) {
+								m_logger.log(Level.SEVERE, e.getMessage(), e);
+							}
+							
+						}
+					}
+				}
+				this.m_app.updateViews(true);
 			}
 		}
 	}
