a7a34dbaa98f1dab53a4d789cdac16286f886ba2
Thilo Brandt <mail@thilo-brandt.de>
2013-05-14 00:58:17 +0200

    [5.0.40] [core] view filter on journal selection via configuration page (emergency configuration)

--- kernel/core/src/ui/de/janrufmonitor/ui/jface/configuration/pages/Journal.java
+++ kernel/core/src/ui/de/janrufmonitor/ui/jface/configuration/pages/Journal.java
@@ -1,11 +1,21 @@
 package de.janrufmonitor.ui.jface.configuration.pages;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Properties;
+
+import org.eclipse.jface.preference.ComboFieldEditor;
+
+import de.janrufmonitor.repository.filter.IFilter;
 import de.janrufmonitor.runtime.IRuntime;
 import de.janrufmonitor.runtime.PIMRuntime;
-import de.janrufmonitor.ui.jface.configuration.AbstractEmptyConfigPage;
+import de.janrufmonitor.ui.jface.application.IFilterManager;
+import de.janrufmonitor.ui.jface.application.journal.JournalFilterManager;
+import de.janrufmonitor.ui.jface.configuration.AbstractFieldEditorConfigPage;
 import de.janrufmonitor.ui.jface.configuration.IConfigPage;
 
-public class Journal extends AbstractEmptyConfigPage {
+public class Journal extends AbstractFieldEditorConfigPage {
 	
     private String NAMESPACE = "ui.jface.configuration.pages.Journal";
     
@@ -31,6 +41,44 @@ public class Journal extends AbstractEmptyConfigPage {
 		if (this.m_runtime==null) 
 			this.m_runtime = PIMRuntime.getInstance();
 		return this.m_runtime;
+	}
+	
+	public String getConfigNamespace() {
+		return "ui.jface.application.journal.Journal";
+	}
+
+	protected void createFieldEditors() {
+		this.setTitle(this.m_i18n.getString(this.getNamespace(), "title", "label", this.m_language));
+
+		this.noDefaultAndApplyButton();
+		
+		List l = new ArrayList();
+		l.add(new IFilter[0]);
+		IFilterManager jfm = new JournalFilterManager();
+		Properties c = getRuntime().getConfigManagerFactory().getConfigManager().getProperties(this.getConfigNamespace());
+		Iterator iter = c.keySet().iterator();
+		String key = null;
+		while (iter.hasNext()) {
+			key = (String)iter.next();
+			if (key.startsWith("filter_")) {
+				String filter = c.getProperty(key);
+				l.add(jfm.getFiltersFromString(filter));
+			}
+		}
+		
+		String[][] filters = new String[l.size()][2];
+		for (int i=0;i<l.size();i++) {
+			filters[i][0] = jfm.getFiltersToLabelText((IFilter[]) l.get(i), 45);
+			filters[i][1] =  jfm.getFiltersToString((IFilter[]) l.get(i));
+		}
+		
+		ComboFieldEditor cfe = new ComboFieldEditor(
+				this.getConfigNamespace()+SEPARATOR+"filter"
+			   , this.m_i18n.getString(getNamespace(), "filter", "label", this.m_language)
+			   , filters, 
+			   this.getFieldEditorParent());
+		addField(cfe);
+		
 	}
 	
 }
