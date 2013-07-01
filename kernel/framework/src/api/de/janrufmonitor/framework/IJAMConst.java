28a50291f4d7231d8f04944abad825f052b2cb8f
Thilo Brandt <mail@thilo-brandt.de>
2013-07-01 18:10:25 +0200

    [5.0.42][core] updated core version and changes.txt

--- kernel/framework/src/api/de/janrufmonitor/framework/IJAMConst.java
+++ kernel/framework/src/api/de/janrufmonitor/framework/IJAMConst.java
@@ -24,12 +24,12 @@ public interface IJAMConst {
 	/**
 	 * Patch level
 	 */
-	public final static String VERSION_PATCH = "41";
+	public final static String VERSION_PATCH = "42";
 	
 	/**
 	 * Build number
 	 */
-	public final static String VERSION_BUILD = "20130701";
+	public final static String VERSION_BUILD = "20130801";
 
 	/**
 	 * Display version
