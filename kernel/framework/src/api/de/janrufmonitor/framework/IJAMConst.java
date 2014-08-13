6ca846a4ca1326e99defcab4c94b73911e130c96
Thilo Brandt <mail@thilo-brandt.de>
2014-08-13 20:22:02 +0200

    [5.0.52][core] adopted version

--- kernel/framework/src/api/de/janrufmonitor/framework/IJAMConst.java
+++ kernel/framework/src/api/de/janrufmonitor/framework/IJAMConst.java
@@ -24,12 +24,12 @@ public interface IJAMConst {
 	/**
 	 * Patch level
 	 */
-	public final static String VERSION_PATCH = "51";
+	public final static String VERSION_PATCH = "52";
 	
 	/**
 	 * Build number
 	 */
-	public final static String VERSION_BUILD = "20140801";
+	public final static String VERSION_BUILD = "20141001";
 
 	/**
 	 * Display version
