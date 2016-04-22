6fe1ece74155c0cbe4457bbcfb87104a73883413
Thilo Brandt <mail@thilo-brandt.de>
2016-04-22 21:07:21 +0200

    [5.0.66][core] added cd command to linux version

--- kernel/core/deployment/linux32/jam.sh
+++ kernel/core/deployment/linux32/jam.sh
@@ -1,2 +1,3 @@
 # !/bash/sh
+cd $(dirname $(readlink -f ${0}))
 java -Djava.library.path=. -cp jamapi.jar:jam.jar:jamlinux.jar:hsqldb.jar:i18n.jar:swt.jar:jffix.jar de.janrufmonitor.application.RunUI
