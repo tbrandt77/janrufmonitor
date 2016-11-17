3a51c1e6fc4d50de4a19b8d45daa0c4c31591d38
Thilo Brandt <mail@thilo-brandt.de>
2016-11-17 16:31:20 +0100

    [5.0.68][core] added new SSL TrustManager to PIMRuntime

--- kernel/core/src/core/de/janrufmonitor/runtime/PIMRuntime.java
+++ kernel/core/src/core/de/janrufmonitor/runtime/PIMRuntime.java
@@ -2,12 +2,22 @@ package de.janrufmonitor.runtime;
 
 import java.net.Authenticator;
 import java.net.PasswordAuthentication;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import de.janrufmonitor.classloader.JamCacheMasterClassLoader;
 import de.janrufmonitor.framework.CallFactory;
@@ -258,6 +268,42 @@ public class PIMRuntime implements IRuntime, IEventSender {
 			this.m_logger.info("jAnrufmonitor working directory: "
 				+ PathResolver.getInstance(this).getInstallDirectory());
 		}
+		
+		try
+	    {
+	        // Create a trust manager that does not validate certificate chains
+	        TrustManager[] trustAllCerts = new TrustManager[] {new X509TrustManager() {
+	            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+	                return null;
+	            }
+	            public void checkClientTrusted(X509Certificate[] certs, String authType) {
+	            }
+	            public void checkServerTrusted(X509Certificate[] certs, String authType) {
+	            }
+	        }
+	        };
+
+	        // Install the all-trusting trust manager
+	        SSLContext sc = SSLContext.getInstance("SSL");
+	        sc.init(null, trustAllCerts, new java.security.SecureRandom());
+	        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+	        // Create all-trusting host name verifier
+	        HostnameVerifier allHostsValid = new HostnameVerifier() {
+	            public boolean verify(String hostname, SSLSession session) {
+	                return true;
+	            }
+	        };
+
+	        // Install the all-trusting host verifier
+	        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+			if (this.m_logger.isLoggable(Level.INFO)) 
+				this.m_logger.info("Added new SSL TrustManager: true");
+	    } catch (NoSuchAlgorithmException e) {
+	        m_logger.log(Level.SEVERE, e.getLocalizedMessage(), e);
+	    } catch (KeyManagementException e) {
+	    	 m_logger.log(Level.SEVERE, e.getLocalizedMessage(), e);
+	    }
 		
 		
 		IRuntime currentRT = PIMRuntime.getInstance();
