3a6bd0c9b021d65c473e2aba808a74f385755010
Thilo Brandt <mail@thilo-brandt.de>
2017-08-20 22:24:39 +0200

    [5.0.72][core] changed version number

--- kernel/framework/src/api/de/janrufmonitor/framework/IJAMConst.java
+++ kernel/framework/src/api/de/janrufmonitor/framework/IJAMConst.java
@@ -24,12 +24,12 @@ public interface IJAMConst {
 	/**
 	 * Patch level
 	 */
-	public final static String VERSION_PATCH = "71";
+	public final static String VERSION_PATCH = "72";
 	
 	/**
 	 * Build number
 	 */
-	public final static String VERSION_BUILD = "20170701";
+	public final static String VERSION_BUILD = "20170820";
 
 	/**
 	 * Display version
