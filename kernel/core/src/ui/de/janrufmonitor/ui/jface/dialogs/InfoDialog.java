2f47527ac6ca7e5565e88169d2427a885f6e3b3f
Thilo Brandt <mail@thilo-brandt.de>
2018-01-04 21:02:24 +0100

    [5.0.74][core] changed version number and log

--- kernel/core/src/ui/de/janrufmonitor/ui/jface/dialogs/InfoDialog.java
+++ kernel/core/src/ui/de/janrufmonitor/ui/jface/dialogs/InfoDialog.java
@@ -256,7 +256,7 @@ public class InfoDialog extends TitleAreaDialog {
 		
 		l = new Label(composite, SWT.RIGHT);
 		l.setText(
-			"(c) 2008 - 2017 by Thilo Brandt      "
+			"(c) 2008 - 2018 by Thilo Brandt      "
 		);
 		l.setLayoutData(gd);
 		
--- kernel/framework/src/api/de/janrufmonitor/framework/IJAMConst.java
+++ kernel/framework/src/api/de/janrufmonitor/framework/IJAMConst.java
@@ -24,12 +24,12 @@ public interface IJAMConst {
 	/**
 	 * Patch level
 	 */
-	public final static String VERSION_PATCH = "73";
+	public final static String VERSION_PATCH = "74";
 	
 	/**
 	 * Build number
 	 */
-	public final static String VERSION_BUILD = "20171106";
+	public final static String VERSION_BUILD = "20180105";
 
 	/**
 	 * Display version
