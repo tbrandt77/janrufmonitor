e5c1c170bea9c58dc41f6f3546242cec3816ff9f
Thilo Brandt <thilo.brandt@me.com>
2018-05-10 22:03:06 +0200

    [5.0.77][core] adjusted version

--- kernel/framework/src/api/de/janrufmonitor/framework/IJAMConst.java
+++ kernel/framework/src/api/de/janrufmonitor/framework/IJAMConst.java
@@ -24,12 +24,12 @@ public interface IJAMConst {
 	/**
 	 * Patch level
 	 */
-	public final static String VERSION_PATCH = "76";
+	public final static String VERSION_PATCH = "77";
 	
 	/**
 	 * Build number
 	 */
-	public final static String VERSION_BUILD = "20180402";
+	public final static String VERSION_BUILD = "20180512";
 
 	/**
 	 * Display version
