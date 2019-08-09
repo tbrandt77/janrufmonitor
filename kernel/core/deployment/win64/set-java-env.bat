5b5357ebc9d4b9f2afd76c3318ffadeb1910a634
Thilo Brandt <thilo.brandt@me.com>
2019-08-09 14:46:08 +0200

    [5.0.82][core] adopted set-java-env.bat for Win64

--- kernel/core/deployment/win64/set-java-env.bat
+++ kernel/core/deployment/win64/set-java-env.bat
@@ -7,41 +7,47 @@ set JDK_HOME=
 :settings
 
 :getjrelocation
- rem Resolve location of Java runtime environment
- set KeyName=HKEY_LOCAL_MACHINE\SOFTWARE\JavaSoft\Java Runtime Environment
- set Cmd=reg query "%KeyName%" /s
- for /f "tokens=2*" %%i in ('%Cmd% ^| find "JavaHome"') do set JRE_HOME=%%j
+rem Resolve location of Java runtime environment
+set KeyName=HKEY_LOCAL_MACHINE\SOFTWARE\JavaSoft\Java Runtime Environment
+set Cmd=reg query "%KeyName%" /s
+for /f "tokens=2*" %%i in ('%Cmd% ^| find "JavaHome"') do set JRE_HOME=%%j
+
+:getjrelocationWOW6432Node
+rem Resolve location of Java runtime environment on WOW6432Node
+set KeyName=HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\JavaSoft\Java Runtime Environment
+set Cmd=reg query "%KeyName%" /s
+for /f "tokens=2*" %%i in ('%Cmd% ^| find "JavaHome"') do set JRE_HOME=%%j
 
 :getjdklocation
- rem Resolve location of Java JDK environment
- set KeyName=HKEY_LOCAL_MACHINE\SOFTWARE\JavaSoft\Java Development Kit
- set Cmd=reg query "%KeyName%" /s
- for /f "tokens=2*" %%i in ('%Cmd% ^| find "JavaHome"') do set JDK_HOME=%%j
+rem Resolve location of Java JDK environment
+set KeyName=HKEY_LOCAL_MACHINE\SOFTWARE\JavaSoft\Java Development Kit
+set Cmd=reg query "%KeyName%" /s
+for /f "tokens=2*" %%i in ('%Cmd% ^| find "JavaHome"') do set JDK_HOME=%%j
 
 :setsysenv
- rem Check if we located either of the Java environments
- if "%JRE_HOME%%JDK_HOME%"=="" goto errornojava
+rem Check if we located either of the Java environments
+if "%JRE_HOME%%JDK_HOME%"=="" goto errornojava
 
- rem If a Java runtime environment is located then set both JAVA_HOME and JAVA_JRE system environment variables
- if not "%JRE_HOME%"=="" (
- echo Java Runtime environment found setting JAVA_HOME and JAVA_JRE to "%JRE_HOME%"
- setx JAVA_HOME "%JRE_HOME%"
- setx JAVA_JRE "%JRE_HOME%"
- setx PATH "%JRE_HOME%\bin;%PATH%"
- )
+rem If a Java runtime environment is located then set both JAVA_HOME and JAVA_JRE system environment variables
+if not "%JRE_HOME%"=="" (
+echo Java Runtime environment found setting JAVA_HOME and JAVA_JRE to "%JRE_HOME%"
+setx JAVA_HOME "%JRE_HOME%"
+setx JAVA_JRE "%JRE_HOME%"
+setx PATH "%JRE_HOME%\bin;%PATH%"
+)
 
- rem If a Java JDK environment is located then set JAVA_HOME system environment variables
- if not "%JDK_HOME%"=="" (
- echo Java JDK environment found setting JAVA_HOME "%JDK_HOME%"
- setx JAVA_HOME "%JDK_HOME%"
- setx PATH "%JDK_HOME%\bin;%PATH%"
- )
+rem If a Java JDK environment is located then set JAVA_HOME system environment variables
+if not "%JDK_HOME%"=="" (
+echo Java JDK environment found setting JAVA_HOME "%JDK_HOME%"
+setx JAVA_HOME "%JDK_HOME%"
+setx PATH "%JDK_HOME%\bin;%PATH%"
+)
 
- goto end
- 
+goto end
+
 :errornojava
- echo Failed to locate any installed java environments event tried locating a JDK, please install a Java Runtime Evnironment or JDK
- goto end
- 
+echo Failed to locate any installed java environments event tried locating a JDK, please install a Java Runtime Evnironment or JDK
+goto end
+
 :end
 endlocal
