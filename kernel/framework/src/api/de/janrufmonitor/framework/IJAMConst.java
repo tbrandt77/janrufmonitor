25be5d2f08dce6b83e4c6c4ebce23845db39a7c9
Thilo Brandt <thilo.brandt@me.com>
2020-09-29 10:22:40 +0200

    [5.0.83][core] adopted version

--- kernel/framework/src/api/de/janrufmonitor/framework/IJAMConst.java
+++ kernel/framework/src/api/de/janrufmonitor/framework/IJAMConst.java
@@ -24,12 +24,12 @@ public interface IJAMConst {
 	/**
 	 * Patch level
 	 */
-	public final static String VERSION_PATCH = "82";
+	public final static String VERSION_PATCH = "83";
 	
 	/**
 	 * Build number
 	 */
-	public final static String VERSION_BUILD = "20200102";
+	public final static String VERSION_BUILD = "20201001";
 
 	/**
 	 * Display version
